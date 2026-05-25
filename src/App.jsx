1b6e6f4af35c85cfb6ec2f790f4bf62dbdf222f1
Talita Lima <talita.lima.tech@gmail.com>
2026-05-25 15:11:57 -0300

    feat:Adicionando novas rotas no app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,27 @@
-// src/App.jsx
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
+import Grupos from './pages/Grupos';
 
 function App() {
   return (
-    <div className="min-h-screen flex flex-col bg-slate-50">
-      <Header />
-      {/* A Home ocupa o meio da tela */}
-      <div className="flex-grow">
-        <Home />
+    <Router>
+      <div className="min-h-screen flex flex-col bg-slate-50 font-sans">
+        <Header />
+        
+        {/* O container das rotas ocupa o espaço entre o header e o footer */}
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/grupos" element={<Grupos />} />
+            {/* Futuramente: <Route path="/cronograma" element={<Cronograma />} /> */}
+          </Routes>
+        </main>
+
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </Router>
   );
 }
 
